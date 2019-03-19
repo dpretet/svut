37f88c4948cde3b6c8e3e9fc3319187937d63db8
Damien Pretet <damien.pretet@me.com>
2019-03-19 17:58:26 +0100

    Add a LAST_STATUS flag, positive is last macro has been asserted

--- svut_h.sv
+++ svut_h.sv
@@ -15,45 +15,54 @@
 
 `ifndef INFO
 `define INFO(msg) \
-    $display("%c[0;37mINFO:     [%g] %s%c[0m", 27, $time, msg, 27)
+        $display("%c[0;37mINFO:     [%g] %s%c[0m", 27, $time, msg, 27)
 `endif
 
 `ifndef SUCCESS
 `define SUCCESS(msg) \
-    $display("%c[0;32mSUCCESS:  [%g] %s%c[0m", 27, $time, msg, 27)
+        $display("%c[0;32mSUCCESS:  [%g] %s%c[0m", 27, $time, msg, 27)
 `endif
 
 `ifndef WARNING
 `define WARNING(msg) \
-    $display("%c[1;33mWARNING:  [%g] %s%c[0m", 27, $time, msg, 27); \
-    svut_warning = svut_warning + 1
+        $display("%c[1;33mWARNING:  [%g] %s%c[0m", 27, $time, msg, 27); \
+		svut_warning += 1
 `endif
 
 `ifndef CRITICAL
 `define CRITICAL(msg) \
-    $display("%c[1;35mCRITICAL: [%g] %s%c[0m", 27, $time, msg, 27); \
-    svut_critical = svut_critical + 1
+        $display("%c[1;35mCRITICAL: [%g] %s%c[0m", 27, $time, msg, 27); \
+		svut_critical += 1
 `endif
 
 `ifndef ERROR
 `define ERROR(msg) \
-    $display("%c[1;31mERROR:    [%g] %s%c[0m", 27, $time, msg, 27); \
-    svut_error = svut_error + 1
+        $display("%c[1;31mERROR:    [%g] %s%c[0m", 27, $time, msg, 27); \
+        svut_error += 1
 `endif
 
 `ifndef SVUT_SETUP
 `define SVUT_SETUP \
+    integer svut_status = 0; \
     integer svut_warning = 0; \
     integer svut_critical = 0; \
     integer svut_error = 0; \
+    integer svut_error_total = 0; \
     integer svut_nb_test = 0; \
-    integer svut_nb_test_success = 0; \
+    integer svut_nb_test_success = 0;
+`endif
+
+`ifndef LAST_STATUS
+`define LAST_STATUS svut_status
 `endif
 
 `ifndef FAIL_IF
 `define FAIL_IF(exp) \
     if (exp) begin \
         `ERROR("FAIL_IF"); \
+        svut_status = 1; \
+    end else begin \
+        svut_status = 0; \
     end
 `endif
 
@@ -61,6 +70,9 @@
 `define FAIL_IF_NOT(exp) \
     if (!exp) begin \
         `ERROR("FAIL_IF_NOT"); \
+        svut_status = 1; \
+    end else begin \
+        svut_status = 0; \
     end
 `endif
 
@@ -68,6 +80,9 @@
 `define FAIL_IF_EQUAL(a,b) \
     if (a === b) begin \
         `ERROR("FAIL_IF_EQUAL"); \
+        svut_status = 1; \
+    end else begin \
+        svut_status = 0; \
     end
 `endif
 
@@ -75,20 +90,28 @@
 `define FAIL_IF_NOT_EQUAL(a,b) \
     if (a !== b) begin \
         `ERROR("FAIL_IF_NOT_EQUAL"); \
+        svut_status = 1; \
+    end else begin \
+        svut_status = 0; \
     end
 `endif
 
+`ifndef UNIT_TESTS
 `define UNIT_TESTS \
     task run(); \
     begin \
-    $display("\n%c[0;36mINFO:     Testsuite execution started%c[0m\n", 27, 27); \
+    $display("\n%c[0;36mINFO:     Testsuite execution started%c[0m\n", 27, 27);
+`endif
 
+`ifndef UNIT_TEST
 `define UNIT_TEST(TESTNAME) \
     begin : TESTNAME \
         setup(); \
         svut_error = 0; \
-        svut_nb_test = svut_nb_test + 1; \
+        svut_nb_test = svut_nb_test + 1;
+`endif
 
+`ifndef UNIT_TEST_END
 `define UNIT_TEST_END \
         #0; \
         teardown(); \
@@ -96,10 +119,13 @@
             svut_nb_test_success = svut_nb_test_success + 1; \
             `SUCCESS("Test successful\n"); \
         end else begin \
+            svut_error_total += svut_error; \
             `ERROR("Test failed\n"); \
         end \
-    end \
+    end
+`endif
 
+`ifndef UNIT_TESTS_END
 `define UNIT_TESTS_END \
     end \
     endtask \
@@ -112,11 +138,15 @@
         if (svut_critical > 0) begin \
             $display("\t  -> %c[1;35mCritical number: %4d%c[0m", 27, svut_critical, 27); \
         end\
+        if (svut_error_total > 0) begin \
+            $display("\t  -> %c[1;31mError number: %4d%c[0m", 27, svut_error_total, 27); \
+        end\
         if (svut_nb_test_success != svut_nb_test) begin \
-            $display("\t  -> %c[1;31mERROR: %3d / %3d tests passed%c[0m\n", 27, svut_nb_test_success, svut_nb_test, 27); \
+            $display("\t  -> %c[1;31mSTATUS: %4d / %4d tests passed%c[0m\n", 27, svut_nb_test_success, svut_nb_test, 27); \
         end else begin \
-            $display("\t  -> %c[0;32mSUCCESS: %3d / %3d tests passed%c[0m\n", 27, svut_nb_test_success, svut_nb_test, 27); \
+            $display("\t  -> %c[0;32mSTATUS: %4d / %4d tests passed%c[0m\n", 27, svut_nb_test_success, svut_nb_test, 27); \
         end \
         $finish(); \
-    end \
+    end
+`endif
 
