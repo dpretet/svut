e1ad087f36bcb7bf33226d4cc7cf5f2933d919ed
Damien Pretet <damien.pretet@me.com>
2019-08-01 11:19:27 +0200

    Remove mention of copyright

--- svutCreate.py
+++ svutCreate.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright 2019 Damien Pretet
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
