71ba657f59e376762a680fe99ad3027a613d748f
Damien Pretet <damien.pretet@me.com>
2019-08-03 12:59:46 +0200

    Renamed testsuite according suffix format

--- test/regression.sh
+++ test/regression.sh
@@ -1,33 +1,45 @@
 #!/usr/bin/env bash
 
+# Get script's location
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # pipe fails if first command fails. Else is always successful
 set -o pipefail
-rm -f regression.txt; touch regression.txt
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-error_num=7
 
-$DIR/../svutRun -test "Adder_unit_test_OK.sv" | tee -a regression.txt
+
+# First testsuite running over the adder, supposed to be successful
+
+echo "INFO: Start Adder OK testsuite"
+"$DIR/../svutRun" -test "Adder_OK_testsuite.sv" | tee log
 ret=$?
 
 if [[ $ret != 0 ]]; then
     echo "Execution failed"
+    exit 1
 else
     echo "Execution completed successfully"
 fi
 
-$DIR/../svutRun -test "Adder_unit_test_KO.sv" | tee -a regression.txt
+
+# Second testsuite running over the adder, supposed to fail
+
+echo "INFO: Start Adder KO testsuite"
+"$DIR/../svutRun" -test "Adder_KO_testsuite.sv" | tee log
 ret=$?
-ec=$(grep -c "ERROR:" regression.txt)
+
+error_num=7
+# Count number of errors
+ec=$(grep -c "ERROR:" log)
 
 if [[ $ret == 0 ]]; then
     if [[ $ec != "$error_num" ]]; then
         echo "Execution suffered $ec issues but not $error_num as expected"
         exit 1
-    else
-        echo "Regression finished successfully"
-        exit 0
     fi
 else
     echo "No errors detected while several exepected"
     exit 1
 fi
+
+echo "Regression finished successfully"
+exit 0
