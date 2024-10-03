e4381f6c81c092d90ee6a41813dc34ff8f7e6c0f
Damien Pretet <damien.pretet@me.com>
2024-10-03 21:31:57 +0200

    Prepare tag v1.9.3

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="svut",
-    version="1.9.2",
+    version="1.9.3",
     description="SystemVerilog Unit Test (SVUT)",
     long_description=long_description,
     long_description_content_type="text/markdown",
